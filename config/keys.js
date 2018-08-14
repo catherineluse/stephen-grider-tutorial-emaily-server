e41842ec18b3fbc5ed9f2b754f36459dfd8775dd
Catherine Luse <you@example.com>
2018-08-14 16:39:46 -0700

    Finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+
+
+if (process.env.NODE_ENG === 'production'){
+  //we are in production - return the prod set of keys
+  module.exports = require('./prod');
+} else {
+  // we are in development - return the dev set of keys
+  module.exports = require('./dev');
+}
---
+++ config/prod.js
@@ -0,0 +1,16 @@
+module.exports = {
+  googleClientID : process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+}
+
+
+//mongodb
+//mongodb://<dbuser>:<dbpassword>@ds221228.mlab.com:21228/emaily-prod1
+
+//google client ID
+//472707569077-cjm4cdrfae97a03v2va4n56ifhotgcpf.apps.googleusercontent.com
+
+//google client secret
+//RFzOENWNAgSPJ2oLU1AvG4vt
