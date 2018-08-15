645fc3d7a7a4fe2388583f59d9de5a1d6ffa3e3e
Catherine Luse <you@example.com>
2018-08-14 17:39:21 -0700

    Remove proxy: true

--- config/prod.js
+++ config/prod.js
@@ -4,13 +4,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 }
-
-
-//mongodb
-//mongodb://<dbuser>:<dbpassword>@ds221228.mlab.com:21228/emaily-prod1
-
-//google client ID
-//472707569077-cjm4cdrfae97a03v2va4n56ifhotgcpf.apps.googleusercontent.com
-
-//google client secret
-//RFzOENWNAgSPJ2oLU1AvG4vt
--- services/passport.js
+++ services/passport.js
@@ -20,8 +20,7 @@ passport.use(new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
-      proxy: true
+      callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id })
@@ -39,35 +38,3 @@ passport.use(new GoogleStrategy(
     }
  )
 );
-
-/*
-accessToken ya29.Glz6BSis7M8zcgKKylrSB8BGYn4_aSL4uVmfuH51NatUaVsp-pZ4BeS3Jb-iJxdU001U3udGV6SOOzPAZj0MvHeHr3OVl32zZl8ZGnV_gLVu0vDRvHROpLnOBF9Oug
-refresh token undefined
-profile: { id: '116343868440478799138',
-  displayName: 'Catherine Luse',
-  name: { familyName: 'Luse', givenName: 'Catherine' },
-  emails: [ { value: 'catherine.luse@gmail.com', type: 'account' } ],
-  photos:
-   [ { value: 'https://lh3.googleusercontent.com/-TC5BBKwy1dQ/AAAAAAAAAAI/AAAAAAAAHd8/4872onxylMI/photo.jpg?sz=50' } ],
-  gender: 'female',
-  provider: 'google',
-  _raw: '{\n "kind": "plus#person",\n "etag": "\\"hCnRu-GwRzXLXPdAHHyDrK_S150/u1y7_ACsKuNsLP92u8eFR3CvZNQ\\"",\n "gender": "female",\n "emails": [\n  {\n   "value": "catherine.luse@gmail.com",\n   "type": "account"\n  }\n ],\n "objectType": "person",\n "id": "116343868440478799138",\n "displayName": "Catherine Luse",\n "name": {\n  "familyName": "Luse",\n  "givenName": "Catherine"\n },\n "url": "https://plus.google.com/116343868440478799138",\n "image": {\n  "url": "https://lh3.googleusercontent.com/-TC5BBKwy1dQ/AAAAAAAAAAI/AAAAAAAAHd8/4872onxylMI/photo.jpg?sz=50",\n  "isDefault": false\n },\n "isPlusUser": true,\n "language": "en",\n "circledByCount": 7,\n "verified": false\n}\n',
-  _json:
-   { kind: 'plus#person',
-     etag: '"hCnRu-GwRzXLXPdAHHyDrK_S150/u1y7_ACsKuNsLP92u8eFR3CvZNQ"',
-     gender: 'female',
-     emails: [ [Object] ],
-     objectType: 'person',
-     id: '116343868440478799138',
-     displayName: 'Catherine Luse',
-     name: { familyName: 'Luse', givenName: 'Catherine' },
-     url: 'https://plus.google.com/116343868440478799138',
-     image:
-      { url: 'https://lh3.googleusercontent.com/-TC5BBKwy1dQ/AAAAAAAAAAI/AAAAAAAAHd8/4872onxylMI/photo.jpg?sz=50',
-        isDefault: false },
-     isPlusUser: true,
-     language: 'en',
-     circledByCount: 7,
-     verified: false } }
-<iframe src='https://players.brightcove.net/665003303001/SJg0bzqkZ_default/index.html?videoId=5617428088001'
-*/
